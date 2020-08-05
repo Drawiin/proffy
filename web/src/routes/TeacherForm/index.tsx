34de0b09a9b33a78959ea25ce06356e4dd4102b4
vinicius santos <viniciussantos@jera.com.br>
2020-08-05 18:16:14 -0400

    quase lá kk

--- web/src/routes/TeacherForm/index.tsx
+++ web/src/routes/TeacherForm/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
+import Header from '../../components/Header'
+
+import './styles.css'
+
 export default function TeacherForm(){
-    return <div>
-        Oiii fomr
-    </div>
+    return (
+        <div id="page-teacher-form" className="container">
+           <Header title="Que incrivel que você quer dar aulas.">
+               
+           </Header>
+        </div>
+    )
 }
--- web/src/routes/TeacherList/index.tsx
+++ web/src/routes/TeacherList/index.tsx
@@ -1,26 +1,51 @@
 import React from 'react'
 
 import './styles.css'
-import { Link } from 'react-router-dom'
+import Header from '../../components/Header'
 
-import logo from '../../assets/images/logo.svg'
-import back from '../../assets/images/icons/back.svg'
 
 export default function TeacherList(){
     return (
         <div id="page-teacher-list" className="container">
-            <header className="page-header">
-                <div className="top-bar-container">
-                    <Link to="/">
-                        <img src={back} alt="voltar"/>
-                    </Link>
-                    <img src={logo} alt="Proffy"/>
-                </div>
+           <Header title={"Esses são os proffys disponíveis."}>
+                <form id="search-teacher">
+                   <div className="input-block">
+                       <label htmlFor="subject">Matéria</label>
+                       <input type="text" id="subject"/>
+                   </div>
+                   
+                   <div className="input-block">
+                       <label htmlFor="week_day">Dia da semana</label>
+                       <input type="text" id="week_day"/>
+                   </div>
+ 
+                   <div className="input-block">
+                       <label htmlFor="time">Hora</label>
+                       <input type="text" id="time"/>
+                   </div>
+               </form>
+           </Header>
 
-                <div className="header-content">
-                    <strong>Esses são os proffys disponíveis.</strong>
-                </div>
-            </header>
+           <main>
+               <article className="teacher-item">
+                   <header>
+                        <img 
+                            src="https://pbs.twimg.com/profile_images/1284301683380625409/9aNFXowQ_400x400.jpg"
+                            alt="avatar"
+                        />
+
+                        <div>
+                            <strong>Vinicius Santos</strong>
+                            <span>Biologia</span>
+                        </div>
+                   </header>
+                   <p>
+                       Entusiasta das mais modernas descobertas em microbiologia
+                   </p>
+                   <br/><br/>
+                   Apaixonado pelas maravilhas que a a natureza e a evolução nos proporcionam
+               </article>
+           </main>
         </div>
     )
 }
