2d52ec66ea08969948538637dd0e28e104705445
vinicius santos <viniciussantos@jera.com.br>
2020-08-06 20:37:53 -0400

    hey

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -26,7 +26,7 @@ export default class ClassesController {
     const timeInMinutes = convertHourToMinutes(time);
 
     const classes = await db('classes')
-      .whereExists(function() {
+      .whereExists(function() { //????
         this.select('class_schedule.*')
           .from('class_schedule')
           .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,16 @@
 import express from 'express'
 import ClassesController from './controllers/ClassesController'
+import ConnectionsController from './controllers/ConnectionsController'
 
 const routes = express.Router()
 const classController =  new ClassesController()
+const connectionsController = new ConnectionsController()
 
 routes.post('/classes', classController.create)
-routes.post('/classes', classController.index)
+routes.get('/classes', classController.index)
+
+routes.post('/connections', connectionsController.create)
+routes.get('/connections', connectionsController.index)
+
 
 export default routes
